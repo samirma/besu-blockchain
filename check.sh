610058d952b11acc623c1c8b03075a38c402c5df
Samir Moreira Antonio <samir.ma@gmail.com>
2024-11-02 14:57:53 +0000

    Update and rename check_validators.sh to check.sh

---
+++ check.sh
@@ -0,0 +1,2 @@
+curl -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' localhost:8545
+curl -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_chainId","params":[],"id":1}' localhost:8545
--- check_validators.sh
+++
@@ -1,1 +0,0 @@
-curl -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' localhost:8545
